ea412393ba0109d16a493b31b5cc8512fb491247
Bangwahyu14 <whagung553@gmail.com>
2024-03-29 14:54:43 +0700

    membuat void display

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -79,3 +79,9 @@ void q_short(int low, int high)
 
 	q_short(j + 1, high);
 }
+
+void display() {
+	cout << "\n-------------------" << endl;
+	cout << "Sorted Array" << endl;
+	cout << "---------------------" << endl;
+}
